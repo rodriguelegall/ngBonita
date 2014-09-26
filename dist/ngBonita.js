e8384977e2ce7097d00d39a7a9939b4aa288b17c
pozil <philippe.ozil@bonitasoft.com>
2014-09-26 18:08:11 +0200

    Updated dist

--- dist/ngBonita.js
+++ dist/ngBonita.js
@@ -34,7 +34,7 @@ angular.module('ngBonita').run(function ($cookies) {
 /**
  * Factory that manages Bonita authentication
  */
-angular.module('ngBonita').factory('bonitaAuthentication', function ($log, $http, $cookies, $q, BonitaSession) {
+angular.module('ngBonita').factory('bonitaAuthentication', function ($log, $http, $cookies, $q, BonitaSession, bonitaUtils) {
 
 	var bonitaAuthentication = {};
 
@@ -87,7 +87,7 @@ angular.module('ngBonita').factory('bonitaAuthentication', function ($log, $http
 		$http({
 			method : 'POST',
 			url : $cookies.bonitaUrl + '/loginservice',
-			data : $.param({
+			data : bonitaUtils.serializeData({
 				username : username,
 				password : password,
 				redirect : false
@@ -135,7 +135,7 @@ angular.module('ngBonita').factory('bonitaAuthentication', function ($log, $http
 		$http({
 			method : 'GET',
 			url : $cookies.bonitaUrl + '/logoutservice',
-			data : $.param({
+			data : bonitaUtils.serializeData({
 				redirect : false
 			}),
 			headers : {
@@ -189,6 +189,35 @@ angular.module('ngBonita').factory('bonitaUtils', function ($http) {
 		return [ paginateResponse ].concat($http.defaults.transformResponse);
 	};
 
+	/**
+	* Serializes data into an URI format (credit: Sudhir from stackoverflow)
+	*/
+	api.serializeData = function (data) {
+		// If this is not an object, defer to native stringification.
+		if (!angular.isObject(data)) {
+			return (data === null) ? '' : data.toString(); 
+		}
+		
+		var buffer = [];
+
+		// Serialize each key in the object.
+		for (var name in data) { 
+			if (!data.hasOwnProperty(name)) {
+				continue; 
+			}
+
+			var value = data[name];
+
+			buffer.push(
+				encodeURIComponent(name) + '=' + encodeURIComponent((value === null) ? '' : value)
+			); 
+		}
+
+		// Serialize the buffer and clean it up for transportation.
+		var source = buffer.join('&').replace(/%20/g, '+'); 
+		return (source); 
+	};
+	
 	return api;
 });
 
